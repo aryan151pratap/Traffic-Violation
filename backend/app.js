8b6ccb0ac1d49067f551f65d32a56957d541439d
Aryan Pratap <apratapsingh151@gmail.com>
2025-08-23 16:04:51 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://traffic-violation.onrender.com'],
   credentials: true,
 }));
 
